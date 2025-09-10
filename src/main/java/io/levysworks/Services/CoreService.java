0760619a33d6bb81b949adefe7ff24654cf381fc
Levy-Y <65444028+Levy-Y@users.noreply.github.com>
2025-09-10 23:40:06 +0200

    CoreService implementation, and refactoring work in progress.

--- src/main/java/io/levysworks/Services/CoreService.java
+++ src/main/java/io/levysworks/Services/CoreService.java
@@ -1,31 +1,63 @@
-package io.levysworks.Services;
+ package io.levysworks.Services;
 
-import io.levysworks.Algorithms.PolygonCoverageAlgorithm;
-import io.levysworks.Configs.ApplicationConfig;
-import io.levysworks.Managers.Database.DatabaseManager;
-import io.levysworks.Managers.IoTCore.IotDataPlaneManager;
-import io.levysworks.Managers.IoTCore.IotManager;
-import io.levysworks.Managers.S3.StorageManager;
-import io.levysworks.Managers.SQS.QueueManager;
-import io.levysworks.Models.IoTCertContainer;
-import io.quarkus.runtime.Startup;
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
-import org.postgis.Geometry;
+ import io.levysworks.Algorithms.PolygonCoverageAlgorithm;
+ import io.levysworks.Configs.ApplicationConfig;
+ import io.levysworks.Managers.Database.DatabaseManager;
+ import io.levysworks.Managers.Database.DbModels.DbDrone;
+ import io.levysworks.Managers.Database.Mappers.CoordinatorMapper;
+ import io.levysworks.Managers.Database.Mappers.DroneMapper;
+ import io.levysworks.Managers.IoTCore.IotDataPlaneManager;
+ import io.levysworks.Managers.IoTCore.IotManager;
+ import io.levysworks.Managers.S3.StorageManager;
+ import io.levysworks.Managers.SQS.QueueManager;
+ import io.levysworks.Models.IoTCertContainer;
+ import jakarta.annotation.PostConstruct;
+ import jakarta.enterprise.context.ApplicationScoped;
+ import jakarta.inject.Inject;
+ import java.io.File;
+ import java.io.IOException;
+ import java.sql.Timestamp;
+ import java.util.Map;
+ import java.util.UUID;
+ import org.postgis.Geometry;
 
-@Startup
-@ApplicationScoped
-public class CoreService {
+// @Startup
+ @ApplicationScoped
+ public class CoreService {
     @Inject IotManager iotManager;
     @Inject IotDataPlaneManager iotDataPlaneManager;
     @Inject DatabaseManager dbManager;
     @Inject QueueManager queueManager;
     @Inject StorageManager storageManager;
     @Inject ApplicationConfig config;
+
+    @Inject DroneMapper droneMapper;
+
+    @Inject CoordinatorMapper coordinatorMapper;
+
     DatabaseManager databaseManager;
+
+    @PostConstruct
+    public void init() {
+        DbDrone drone = droneMapper.findByUuid(UUID.randomUUID());
+        if (drone == null) {
+            System.err.println("Could not find drone with uuid: " + UUID.randomUUID());
+        } else {
+            System.out.println(drone.getAddress());
+        }
+
+        try {
+            coordinatorMapper.insert(
+                    UUID.randomUUID(),
+                    "345df456dfg454",
+                    "Teszt",
+                    "Elek",
+                    "teszt.elek@gmail.com",
+                    new Timestamp(System.currentTimeMillis()));
+        } catch (Exception ex) {
+            System.err.printf("Couldn't create new coordinator: %s", ex.getMessage());
+        }
+    }
 
     // TODO: Implement underlying methods for database operations
     public void registerNewDrone(
@@ -35,14 +67,17 @@ public class CoreService {
             String address,
             String px4Version,
             String agentVersion) {
+        UUID uuid = UUID.randomUUID();
+
         IoTCertContainer certContainer = iotManager.generateCertificate();
         iotManager.createDevice(droneName, outpost, px4Version, agentVersion);
         iotManager.attachCertificate(droneName, certContainer.getCertificateARN());
 
+        // TODO: Refactor to use a single group identifier, group name or UUID
+
         iotManager.addDeviceToGroup(droneName, group);
 
-        String groupUUID = dbManager.getGroupUUID(droneName);
-        dbManager.addDrone(droneName, groupUUID, address, agentVersion);
+//        droneMapper.insertDrone(uuid, droneName, groupUUID, address, agentVersion);
     }
 
     public void createNewMission(String outpost, String group, String coordinatorUUID)
@@ -57,8 +92,7 @@ public class CoreService {
 
         String groupARN = iotManager.getGroupARN(group);
 
-        // Create 'Download-File' mission -> download mission file from url and execute mission from
-        // it
+        // Create 'Download-File' mission -> download mission file from url and execute mission from it
         iotManager.createIoTJob(
                 missionName,
                 groupARN,
@@ -67,9 +101,9 @@ public class CoreService {
                         Map.entry("downloadUrl", presignedUrl),
                         Map.entry("filePath", "/tmp/missions/")));
 
-        String groupUUID = databaseManager.getGroupUUID(group);
-        String bundleUrl = storageManager.getInternalObjectUrl(key);
-
-        databaseManager.createMission(groupUUID, missionName, bundleUrl, coordinatorUUID);
+//        String groupUUID = databaseManager.getGroupUUID(group);
+//        String bundleUrl = storageManager.getInternalObjectUrl(key);
+//
+//        databaseManager.createMission(groupUUID, missionName, bundleUrl, coordinatorUUID);
     }
-}
+ }
