60300ad73db3facec68fee86e5311d9e910040f3
Levy-Y <65444028+Levy-Y@users.noreply.github.com>
2025-09-13 19:22:10 +0200

    Implemented CoreService#registerNewDrone. Modified IoTCertContainer to include the private key for future usage. Implemented policy creation and attaching.

--- src/main/java/io/levysworks/Services/CoreService.java
+++ src/main/java/io/levysworks/Services/CoreService.java
@@ -1,30 +1,29 @@
- package io.levysworks.Services;
+package io.levysworks.Services;
 
- import io.levysworks.Algorithms.PolygonCoverageAlgorithm;
- import io.levysworks.Configs.ApplicationConfig;
- import io.levysworks.Managers.Database.DatabaseManager;
- import io.levysworks.Managers.Database.DbModels.DbDrone;
- import io.levysworks.Managers.Database.Mappers.CoordinatorMapper;
- import io.levysworks.Managers.Database.Mappers.DroneMapper;
- import io.levysworks.Managers.Database.Mappers.GroupMapper;
- import io.levysworks.Managers.Database.Mappers.MissionMapper;
- import io.levysworks.Managers.IoTCore.IotDataPlaneManager;
- import io.levysworks.Managers.IoTCore.IotManager;
- import io.levysworks.Managers.S3.StorageManager;
- import io.levysworks.Models.IoTCertContainer;
- import jakarta.annotation.PostConstruct;
- import jakarta.enterprise.context.ApplicationScoped;
- import jakarta.inject.Inject;
- import java.io.File;
- import java.io.IOException;
- import java.sql.Timestamp;
- import java.util.Map;
- import java.util.UUID;
- import org.postgis.Geometry;
+import io.levysworks.Algorithms.PolygonCoverageAlgorithm;
+import io.levysworks.Configs.ApplicationConfig;
+import io.levysworks.Managers.Database.DatabaseManager;
+import io.levysworks.Managers.Database.Mappers.CoordinatorMapper;
+import io.levysworks.Managers.Database.Mappers.DroneMapper;
+import io.levysworks.Managers.Database.Mappers.GroupMapper;
+import io.levysworks.Managers.Database.Mappers.MissionMapper;
+import io.levysworks.Managers.IoTCore.IotDataPlaneManager;
+import io.levysworks.Managers.IoTCore.IotManager;
+import io.levysworks.Managers.S3.StorageManager;
+import io.levysworks.Models.IoTCertContainer;
+import io.quarkus.runtime.Startup;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Map;
+import java.util.UUID;
+import org.postgis.Geometry;
 
-// @Startup
- @ApplicationScoped
- public class CoreService {
+@Startup
+@ApplicationScoped
+public class CoreService {
     @Inject IotManager iotManager;
     @Inject IotDataPlaneManager iotDataPlaneManager;
     @Inject DatabaseManager dbManager;
@@ -38,30 +37,9 @@
 
     DatabaseManager databaseManager;
 
-    @PostConstruct
-    public void init() {
-        DbDrone drone = droneMapper.findByUuid(UUID.randomUUID());
-        if (drone == null) {
-            System.err.println("Could not find drone with uuid: " + UUID.randomUUID());
-        } else {
-            System.out.println(drone.getAddress());
-        }
-
-        try {
-            coordinatorMapper.insert(
-                    UUID.randomUUID(),
-                    "345df456dfg454",
-                    "Teszt",
-                    "Elek",
-                    "teszt.elek@gmail.com",
-                    new Timestamp(System.currentTimeMillis()));
-        } catch (Exception ex) {
-            System.err.printf("Couldn't create new coordinator: %s", ex.getMessage());
-        }
-    }
-
     /**
      * Groups multiple manager operations to register a new drone in IoT Core, and RDS
+     *
      * @param outpost Name of the outpost to create the drone in
      * @param group The name of the groups to create the drone in
      * @param droneName Desired name of the drone
@@ -69,7 +47,7 @@
      * @param px4Version PX4 firmware version running on the pixhawk hardware
      * @param agentVersion Version of the OnboardAgent client
      */
-    public void registerNewDrone(
+    public IoTCertContainer registerNewDrone(
             String outpost,
             String group,
             String droneName,
@@ -80,6 +58,10 @@
 
         IoTCertContainer certContainer = iotManager.generateCertificate();
         iotManager.createDevice(droneName, outpost, px4Version, agentVersion);
+
+        String policyName = iotManager.createPolicy(droneName);
+        iotManager.attachPolicyToCertificate(certContainer.getCertificateARN(), policyName);
+
         iotManager.attachCertificate(droneName, certContainer.getCertificateARN());
 
         String groupARN = iotManager.getGroupARN(group);
@@ -88,10 +70,13 @@
         UUID groupUUID = groupMapper.findByName(group).getOutpost_uuid();
         Timestamp addedDate = new Timestamp(System.currentTimeMillis());
         droneMapper.insertDrone(uuid, droneName, groupUUID, address, agentVersion, addedDate);
+
+        return certContainer;
     }
 
     /**
      * Coordinates multiple managers to create an IoT Core mission for a group of drones
+     *
      * @param outpost The name of the outpost the groups is in
      * @param group Name of the group of drones
      * @param coordinatorUUID The UUID of the coordinator performing this operation
@@ -100,7 +85,7 @@
     public void createNewMission(String outpost, String group, String coordinatorUUID)
             throws IOException {
         Timestamp startedAt = new Timestamp(System.currentTimeMillis());
-        String missionName = "mission-" + outpost + ":" + group + "-" + startedAt.toString();
+        String missionName = "missions/" + outpost + "/" + group + "/mission-" + startedAt;
 
         Geometry area = dbManager.getOutpostGeometry(outpost);
 
@@ -110,7 +95,8 @@
 
         String groupARN = iotManager.getGroupARN(group);
 
-        // Create 'Download-File' mission -> download mission file from url and execute mission from it
+        // Create 'Download-File' mission -> download mission file from url and execute mission from
+        // it
         iotManager.createIoTJob(
                 missionName,
                 groupARN,
@@ -122,6 +108,12 @@
         UUID groupUUID = groupMapper.findByName(group).getOutpost_uuid();
         String bundleUrl = storageManager.getInternalObjectUrl(key);
 
-        missionMapper.insert(UUID.randomUUID(), groupUUID, missionName, bundleUrl, startedAt, UUID.fromString(coordinatorUUID));
+        missionMapper.insert(
+                UUID.randomUUID(),
+                groupUUID,
+                missionName,
+                bundleUrl,
+                startedAt,
+                UUID.fromString(coordinatorUUID));
     }
- }
+}
